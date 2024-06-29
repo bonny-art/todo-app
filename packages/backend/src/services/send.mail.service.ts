973786e32a0f7e179697624dddbbb55e37bfa1a0
bonny-art <svitlana.otenko@gmail.com>
2024-06-29 17:12:39 +0300

    feat: Switch email service to Meta mailing

--- packages/backend/src/services/send.mail.service.ts
+++ packages/backend/src/services/send.mail.service.ts
@@ -1,19 +1,30 @@
 import { EmailLetterT } from '@/types/auth.type';
 import nodemailer, { Transporter } from 'nodemailer';
 
-const { META_EMAIL, MAILTRAP_USER, MAILTRAP_PASS } = process.env;
+const { META_EMAIL, META_PASSWORD } = process.env;
 
 class SendEmailService {
 	private transporter: Transporter;
 
 	constructor() {
+		// const nodemailerConfig = {
+		// 	host: 'sandbox.smtp.mailtrap.io',
+		// 	port: 2525,
+		// 	auth: {
+		// 		user: MAILTRAP_USER,
+		// 		pass: MAILTRAP_PASS,
+		// 	},
+		// };
+
 		const nodemailerConfig = {
-			host: 'sandbox.smtp.mailtrap.io',
-			port: 2525,
+			host: 'smtp.meta.ua',
+			port: 465,
+			secure: true,
 			auth: {
-				user: MAILTRAP_USER,
-				pass: MAILTRAP_PASS,
+				user: META_EMAIL,
+				pass: META_PASSWORD,
 			},
+			tls: { rejectUnauthorized: false },
 		};
 
 		this.transporter = nodemailer.createTransport(nodemailerConfig);
